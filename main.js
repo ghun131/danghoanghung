608311177d5a5b2ac8a82cee38010d61553e3360
hungHoangDang <33661599+hungHoangDang@users.noreply.github.com>
2018-02-24 16:05:18 +0700

    update files

---
+++ main.js
@@ -0,0 +1,15 @@
+//smooth scroll the page when click on button on navbar
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(e){
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+});
